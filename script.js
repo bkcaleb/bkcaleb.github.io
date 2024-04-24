a2771bae7a65d6d8f7d3543d89a60f4e260e3cc9
Caleb Lee <bkcaleb@gmail.com>
2024-04-23 17:17:23 -0700

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("left").addEventListener("click", function() {
+    window.location.href = "left_page.html";
+});
+
+document.getElementById("right").addEventListener("click", function() {
+    window.location.href = "right_page.html";
+});
