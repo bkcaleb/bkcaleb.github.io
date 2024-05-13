7d21a6c7e1aba50149fd4addd1d0c28e39b14426
Caleb Lee <bkcaleb@gmail.com>
2024-05-13 16:45:05 -0700

    no more lang selection

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("left").addEventListener("click", function() {
-    window.location.href = "left_page.html";
-});
-
-document.getElementById("right").addEventListener("click", function() {
-    window.location.href = "right_page.html";
-});
